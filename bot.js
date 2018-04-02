9bf95e155fdfc6c52a7f1a1814d9d10851d833b0
plopez01 <37042253+plopez01@users.noreply.github.com>
2018-04-02 17:45:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,12 @@ client.on('ready', () => {
 	client.user.setActivity('Surviving the storm')
 	console.log("Bot iniciado correctamente!");
 });
+
+client.on("message", (message) => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+	if (message.content == "hola") {
+		message.channel.send("hola que tal");
+	}
+	
+});
 client.login(process.env.BOT_TOKEN); //Login del token
