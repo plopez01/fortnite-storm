b9d572210525aea234c483427584aeca1ed910cf
plopez01 <37042253+plopez01@users.noreply.github.com>
2018-04-02 17:37:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on('ready', () => {
+	console.log(`[Start] ${new Date()}`); //Hora a la que se inicio el server
+	client.user.setActivity('Surviving the storm')
+	console.log("Bot iniciado correctamente!");
+});
+client.login(process.env.BOT_TOKEN); //Login del token
