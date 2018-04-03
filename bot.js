ce48db1f0ea34b007bc70ee3871ff6ec61495d8e
plopez01 <37042253+plopez01@users.noreply.github.com>
2018-04-03 20:54:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 client.on("guildMemberAdd", member => {
 	let role = message.guild.roles.get("430801404832120833");
-	let user = message.member;
+	let member = message.member;
 	member.addRole(role).catch(console.error);
 });
 
