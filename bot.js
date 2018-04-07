c74cff76ecd958293fe04913bf857f927dc37c37
plopez01 <37042253+plopez01@users.noreply.github.com>
2018-04-07 20:13:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("guildMemberAdd", member => {
 client.on("message", (message) => {
 	let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
 	if (msg.includes("HTTPS://DISCORD.GG/")) {
-		if (msg.includes("HTTPS://DISCORD.GG/S7QRRJM")){
+		if (!msg.includes("HTTPS://DISCORD.GG/S7QRRJM")){
 			let channelLogs = client.channels.get("428669999188803586");
 			console.log(channelLogs);
         		message.delete(1000);
