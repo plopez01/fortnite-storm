d8acfd4f30003a9afa740dda771c3e2966c9af0a
plopez01 <37042253+plopez01@users.noreply.github.com>
2018-04-20 20:52:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,15 +21,15 @@ client.on("message", (message) => {
 		console.log(usuarios);
 		let a1 = [];
 		for (x = 0; x <= usuarios.length; ++x){
-			if(usuarios[x].roles.has("428649304983470083")){
+			if(usuarios[x]._roles.has("428649304983470083")){
 			a1.push(usuarios[x]);
 			}
 		}
 		for(i = 0; i <= a1.length; ++i){
-			if(!a1[i].roles.has("436953528926208012")){
+			if(!a1[i]._roles.has("436953528926208012")){
 				a1[i].addRole("436953643846074387").catch(console.error);
 				a1[i].removeRole("436952809204744195").catch(console.error);
-				if(a1[i].roles.has("436952752342433802")){
+				if(a1[i]._roles.has("436952752342433802")){
 					a1[i].removeRole("436952752342433802").catch(console.error);
 				}
 			}
